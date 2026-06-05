cf0609a081b204e186ee7e98f7b774ea11d30d33
Lukikitas <lucasmrds97@gmail.com>
2026-06-04 22:32:37 -0300

    hotfix1

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Necesario para GitHub Pages: https://lukikitas.github.io/foto-app/
+  base: '/foto-app/',
   plugins: [react()],
 })
