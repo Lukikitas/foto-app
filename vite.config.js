69588761bdbdc6c5e7252368a8da646a33487200
Lukikitas <lucasmrds97@gmail.com>
2026-06-07 16:49:28 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vite.dev/config/
 export default defineConfig({
   // Necesario para GitHub Pages: https://lukikitas.github.io/foto-app/
-  base: '/foto-app/',
+  base: '/',
   plugins: [
     react(),
     VitePWA({
